52bc3e007a9b1575012451610c09685084e583fb
Nadir Arbia <nadir.arbia@gmail.com>
2017-07-22 21:51:35 +0200

    up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,29 @@
 class ArticlesController < ApplicationController
 
+  include ArticlesHelper
+
   def index
     @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    temp = Article.find(params[:id])
+    temp.destroy
+    redirect_to 'articles#index'#article_path(@article)
+    show
   end
 end
