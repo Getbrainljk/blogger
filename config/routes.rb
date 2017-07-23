6a6b31f1c8a98de42673a28f219cb3bc021d336c
Nadir Arbia <nadir.arbia@gmail.com>
2017-07-23 18:14:53 +0200

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles
-  root to: 'articles#index'
 
+  resources :articles do
+    resources :comments
+  end
+
+  root to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
